b2ce0d750970dd6d19a17ada51c1044b9e3be472
spall <forspall@gmail.com>
2014-01-09 01:54:06 +0700

    initial

--- moderator/protected/models/ModeratorModel.php
+++ moderator/protected/models/ModeratorModel.php
@@ -1,15 +1,12 @@
 <?php
 
-class NodeModel extends CPModel 
+class ModeratorModel extends CPModel 
 {   
-    const STATUS_ENABLED = 'en';
-    const STATUS_DISABLED = 'dis';
     
-    public $ip;
     public $name;
-    public $location;
-    public $status = self::STATUS_ENABLED;
-    public $weight = 1;
+    public $active;
+    public $email;
+ 
 
     public static function model($className = __CLASS__) {
         return parent::model($className);
@@ -17,69 +14,43 @@ class NodeModel extends CPModel
 
     public function getCollectionStructure()
     {
-        return array('name' => 'node');
+        return array('name' => 'moderator');
     }
     
     public function getMongoDBComponent()
     {
-        return Yii::app()->getComponent('dictionarydb');
+        return Yii::app()->getComponent('db');
     }
     
-    public function getListWeight()
-    {
-        return range(0, 20);
-    }
-    
-    public function getViewStatus()
-    {
-        $list = $this->getListStatus();
-        if ($this->status && isset($list[$this->status])) {
-            return $list[$this->status];
-        }
-    
-        return NULL;
-    }
     
     public function indexes()
     {
         return array(
-            'location' => array(
-                'key' => array(
-                    'location' => '2d'
-                )
+            //'location' => array(
+            //    'key' => array(
+            //        'location' => '2d'
+            //    )
             )
         );
     }
     
-    public function getListStatus()
-    {
-        return array(
-                self::STATUS_ENABLED  => 'enabled',
-                self::STATUS_DISABLED => 'disabled'
-        );
-    }
+   
     
     public function attributeLabels() 
     {
         return array(
                 '_id'      => 'ID',
-                'ip'       => 'IP',
                 'name'     => 'Name',
-                'location' => 'Location',
-                'status'   => 'Status',
-                'weight'   => 'Weight',
         );
     }
     
     public function rules() 
     {
         return array(
-            array('ip, name, location', 'required'),
-            array('name', 'uniqueName'),
+            array('name', 'required'),
+            array('email', 'uniqueEmail'),
             array('name', 'url'),
-            array('location', 'LocationValidator'),
-            array('location', 'filter', 'filter' => array('LocationValidator', 'normal')),
-            array('status', 'in', 'range' => array_keys($this->getListStatus())),
+            array('active', 'in', 'range' => array[0,1]),
         );
     }    
     
@@ -89,19 +60,19 @@ class NodeModel extends CPModel
      * @param unknown_type $attribute
      * @param unknown_type $params
      */
-    public function uniqueName($attribute, $params)
+    public function uniqueEmail($attribute, $params)
     {
-        $name = $this->attributes[$attribute];
+        $email = $this->attributes[$attribute];
         
         $criteria = new EMongoCriteria();
-        $criteria->name = $name;
+        $criteria->email = $email;
         
         if (!$this->getIsNewRecord()) {  
             $criteria->_id('!=', $this->_id);
         }
         
         if ($this->count($criteria) ) {
-            $this->addError($attribute, 'Name ' . $name . ' already exists');
+            $this->addError($attribute, 'Email ' . $email . ' already exists');
         }
     }
 }
