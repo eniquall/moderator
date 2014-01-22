2c5b3d0596b999926d9197d325808ed7c7ad8b0f
Dmitriy Navozenko <eniquall@gmail.com>
2014-01-22 23:07:41 +0200

    RestAPI controller

--- application/protected/controllers/RestController.php
+++ application/protected/controllers/RestController.php
@@ -5,14 +5,14 @@
  * Time: 1:58 AM
  */
 
-class RestController {
+class RestController extends Controller{
 	// main action of the rest interface - adds new content and returns info about moderated content by project
 	/**
 	 * @param $apiKey
 	 * @param $data - is JSON object in format
 	 * {
 		id: “1234567890”,   ­ уникальный идентификатор контента,  произвольный набор символов до 128знаков
-		“project” : “nyrby.com”,   ­ идентификатор проекта
+		“projectId” : “123342”,   ­ идентификатор проекта 24 hex of the mondoId object
 		type: “profile”,  ­ тип контента
 		lang: “ru”,   ­ язык (опционально)
 		data: [   ­ содержимое. Произвольный набор строк с указанием типа  text, img, audio, video,
@@ -25,15 +25,36 @@ class RestController {
 		]
 	},
 	{..}
+	 *
+	 *
+	 * http://moderator.local/api/?apiKey=12344567890&data={}
 	 */
 	public function actionIndex($apiKey, $data) {
+		$data = <<<DATA
+[{
+"id": "123",
+"projectId": "70fdf952f7fb67e5177121b9d1530d73",
+"type": "profile",
+"lang": "ru",
+"data": [
+    {"img": "url" },
+    {"text": "я стою на утесе и машу писей ветру!!!"}
+],
+"context": [
+    {"text": "Василий,25"},
+    {"text": "12 жалоб"}
+]
+}]
+DATA;
+
 		$project = $this->getProjectByApiKey($apiKey);
 
 		if (empty($project)) {
-			$this->_generateError(403, 'Project with this apiKey (' . $apiKey , ') was not found. Project doesn\'t exist or inactive');
+			$this->_generateError(403, 'Project with this apiKey (' . $apiKey . ') was not found. Project doesn\'t exist or inactive');
 		}
 
-		$decodedData = (array) CJSON::decode($data);
+		$decodedData = CJSON::decode($data);
+
 		foreach($decodedData as $contentByUser) {
 			// check content json format by each user
 			if ($this->_checkData($contentByUser)) {
@@ -52,26 +73,29 @@ class RestController {
 	public function getProjectByApiKey($apiKey) {
 		$criteria = new EMongoCriteria();
 		$criteria->apiKey = $apiKey;
-		$criteria->isActive = true;
-
+		$criteria->isActive = '1';
 		$project = ProjectModel::model()->find($criteria);
+
 		return $project;
 	}
 
 	protected function _generateError($httpErrorCode, $message) {
-		$this->_respond();
+		throw new CHttpException($httpErrorCode, $message);
+		//$this->_respond();
 	}
 
-	protected function _checkData(stdClass $contentByUser) {
-		if (
-			empty($contentByUser->id) ||
-			empty($contentByUser->project) ||
-			empty($contentByUser->type) ||
-			empty($contentByUser->data) ||
-			!is_array($contentByUser->data)) {
-			Yii::log("Invalide format of user content: " . CJSON::encode($contentByUser), CLogger::LEVEL_ERROR);
+	protected function _checkData($contentByUser) {
+
+		if ( !is_array($contentByUser) ||
+			empty($contentByUser['id']) ||
+			empty($contentByUser['projectId']) ||
+			empty($contentByUser['type']) ||
+			empty($contentByUser['data']) ||
+			!is_array($contentByUser['data'])) {
+			Yii::log("Invalide format of user content: " . CJSON::encode($contentByUser)
+				. ' Check your data request format. id, projectId, type, data are required, data - is an array', CLogger::LEVEL_ERROR);
 			return false;
-			//$this->_generateError(500, 'Check your data request format. id, project, type, data are required, data - is an array');
+			//$this->_generateError(500, 'Check your data request format. id, projectId, type, data are required, data - is an array');
 		}
 	}
 
@@ -94,5 +118,9 @@ class RestController {
 
 			// in_array()
 		}
+	}
+
+	protected function _getModeratedContentByProject() {
+		return true;
 	}
 }
