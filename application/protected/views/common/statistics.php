88d8620d5cbd67a965baba9abe7b0ca2cbb34cde
dmitriyn <dn@boligportal.dk>
2014-01-31 23:52:18 +0200

    lost view

---
+++ application/protected/views/common/statistics.php
@@ -0,0 +1,7 @@
+<h1>Content items moderates</h1>
+
+previus month:
+<?php $this->widget('bootstrap.widgets.TbLabel', array( 'type'=>'info', 'label'=> $prevMonthStat)); ?>
+<br>
+current month
+<?php $this->widget('bootstrap.widgets.TbLabel', array( 'type'=>'info', 'label'=> $curMonthStat)); ?>
