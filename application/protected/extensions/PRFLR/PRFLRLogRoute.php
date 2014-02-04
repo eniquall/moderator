d409a3799b87a8fc80bdb9293f282e6e32e94fe3
Andrey Evsyukov <andrey.evsyukov@gmail.com>
2014-02-04 14:23:16 +0700

    PRFLR Fix

--- application/protected/extensions/PRFLR/PRFLRLogRoute.php
+++ application/protected/extensions/PRFLR/PRFLRLogRoute.php
@@ -5,11 +5,11 @@
  * @note following options should be enabled in config
  *     Yii::getLogger()->autoFlush = 1;
  *     Yii::getLogger()->autoDump = 1;
- * 
+ *
  * @author     Alexey Spiridonov <forspall@gmail.com>
  */
-class PRFLRLogRoute extends CLogRoute {
-
+class PRFLRLogRoute extends CLogRoute
+{
     public $source;
     public $apikey;
 
@@ -23,12 +23,13 @@ class PRFLRLogRoute extends CLogRoute {
     }
 
     /**
-     * Stores log messages 
+     * Stores log messages
      * @param array $logs list of log messages
      */
     protected function processLogs($logs) {
         // work only with first element
         $_log = $logs[0];
+
         // profile log message have begin: and end: prefixes
         if ($_log[1] == 'profile') {
             if ($_log[0][0] == 'b') {
@@ -36,9 +37,8 @@ class PRFLRLogRoute extends CLogRoute {
                 PRFLR::Begin(substr($_log[0], 6, strlen($_log[0])));
             } else {
                 //end
-                PRFLR::End(substr($_log[0], 4, strlen($_log[0])), $_log[2]);
+                PRFLR::End(substr($_log[0], 4, strlen($_log[0])), trim($_log[2]) ? $_log[2] : 'ss_v1');
             }
         }
     }
-
 }
